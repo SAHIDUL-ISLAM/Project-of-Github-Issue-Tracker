d830abb9f62c083df2dce1117047c9029f5af69d
Sahid-Islam <sahidul150264@gmail.com>
2026-03-08 15:14:43 +0600

    fetch all data from all button

--- home.js
+++ home.js
@@ -1,4 +1,7 @@
-const allBtnTab = (id) => {
-    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/${id}`;
-    console.log(url);
+
+async function loadAllData(){
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const data = await res.json();
+    console.log(data);
 }
+loadAllData();
